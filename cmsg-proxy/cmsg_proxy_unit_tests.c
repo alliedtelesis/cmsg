77fe41e44add306cfad511ccb4b091eff6b55930
Matt Bennett <matt.bennett@alliedtelesis.co.nz>
2016-12-06 16:38:03 +1300

    INTSTAT-83: Add unit test framework for cmsg-proxy
    The Makefile.am/configure.ac scripts needed to be updated to use
    pkg-config correctly.
    
    Change-Id: Ifc22fcdca80180d83eef6476686000b9f5f084a2
    Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

---
+++ cmsg-proxy/cmsg_proxy_unit_tests.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2016, Allied Telesis Labs New Zealand, Ltd
+ */
+
+#include <np.h>
+#include "cmsg_proxy.c"
+
+void
+test_cmsg_proxy_dummy (void)
+{
+    NP_ASSERT_TRUE (true == true);
+}
