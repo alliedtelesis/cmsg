d788b4cbd9ccfc4bda5c9a37a749b27aeb231ac5
Matt Bennett <matt.bennett@alliedtelesis.co.nz>
2017-01-23 14:28:11 +1300

    VLANAPI-45: Add cmsg macro to set required fields
    Although this is the same as CMSG_SET_FIELD_PTR create a new function
    so that is explicit what type of field it is setting.
    
    Change-Id: I912858e789396bc1a531a9a92e44104a694beefc
    Reviewed-by: Bernie Harris <bernie.harris@alliedtelesis.co.nz>
    Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

--- cmsg/cmsg.h
+++ cmsg/cmsg.h
@@ -53,6 +53,11 @@ extern ProtobufCAllocator cmsg_memory_allocator;
         (_name)->has_##_field = TRUE;               \
     } while (0)
 
+#define CMSG_SET_REQUIRED_FIELD_VALUE(_name, _field, _value) \
+    do {                                                     \
+        (_name)->_field = (_value);                          \
+    } while (0)
+
 #define CMSG_UNSET_AND_ZERO_FIELD_VALUE(_name, _field) \
     do {                                               \
         (_name)->_field = 0;                           \
