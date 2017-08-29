alliedtelesis/cmsg

commit 0e1c853e138514da27011f7c95b9ef087889d351

VCSAPI-236: Add helper function to update field in received message
This allows replacing a text string in a received message in a way that
can still be freed with CMSG_FREE_RECV_MSG.

This is useful to be able to slightly modify a received message before
sending to another destination.

Implement cmsg_strdup for use inside this function.

Change-Id: Ia1927e01b831e5ddcadf4885d58c13b9516eed07
Reviewed-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/cmsg_private.h
@@ -256,10 +256,12 @@ cmsg_tlv_header_process (uint8_t *buf, cmsg_server_request *server_request,
 #define CMSG_CALLOC(nmemb,size)     cmsg_calloc ((nmemb), (size), __FILE__,  __LINE__)
 #define CMSG_ASPRINTF(strp,fmt,...) cmsg_asprintf (__FILE__,  __LINE__, (strp),\
                                                    (fmt), ##__VA_ARGS__)
+#define CMSG_STRDUP(strp) cmsg_strdup (__FILE__,  __LINE__, (strp))
 #define CMSG_FREE(ptr)              cmsg_free ((ptr),  __FILE__,  __LINE__)
 void *cmsg_malloc (size_t size, const char *filename, int line);
 void *cmsg_calloc (size_t nmemb, size_t size, const char *filename, int line);
 int cmsg_asprintf (const char *filename, int line, char **strp, const char *fmt, ...);
+char *cmsg_strdup (const char *strp, const char *filename, int line);
 void cmsg_free (void *ptr, const char *filename, int line);
 void cmsg_malloc_init (int mtype);
 

cmsg/cmsg.c
@@ -346,6 +346,19 @@ cmsg_asprintf (const char *filename, int line, char **strp, const char *fmt, ...
     return ret;
 }
 
+char *
+cmsg_strdup (const char *strp, const char *filename, int line)
+{
+    char *p = strdup (strp);
+
+    if (cmsg_mtype > 0)
+    {
+        g_mem_record_alloc (p, cmsg_mtype, filename, line);
+    }
+
+    return p;
+}
+
 void *
 cmsg_realloc (void *ptr, size_t size, const char *filename, int line)
 {
@@ -498,6 +511,30 @@ cmsg_repeated_append (void ***msg_ptr_array, size_t *num_elems, const void *ptr,
         new_array_ptr[(*num_elems)++] = (void *) ptr;
         *msg_ptr_array = new_array_ptr;
     }
+}
+
+/**
+ * Free the contents of a string field in a received message, recording that it has been freed.
+ * Then duplicate and record the allocation of the passed in string and set it in the message.
+ * Should be called using CMSG_UPDATE_RECV_MSG_STRING_FIELD macro.
+ * @param field Field that needs to be updated
+ * @param new_val string to put into the field
+ * @param file Calling file
+ * @param line Calling line
+ */
+void
+cmsg_update_recv_msg_string_field (char **field, const char *new_val,
+                                   const char *file, int line)
+{
+    char *strp = NULL;
+    cmsg_free (*field, file, line);
+
+    if (new_val)
+    {
+        strp = cmsg_strdup (new_val, file, line);
+    }
+
+    *field = strp;
 }
 
 static void *

cmsg/cmsg.h
@@ -29,6 +29,9 @@ void cmsg_msg_array_free (void *msg_array, const char *file, int line);
 /* note - use CMSG_REPEATED_APPEND() instead of calling this directly */
 void cmsg_repeated_append (void ***msg_ptr_array, size_t *num_elems, const void *ptr,
                            const char *file, int line);
+/* note - use CMSG_UPDATE_RECV_MSG_STRING_FIELD() instead of calling this directly */
+void cmsg_update_recv_msg_string_field (char **field, const char *new_val,
+                                        const char *file, int line);
 
 extern ProtobufCAllocator cmsg_memory_allocator;
 
@@ -148,6 +151,19 @@ extern ProtobufCAllocator cmsg_memory_allocator;
     if ((_name) && (_name)->_field)                         \
         for (_idx = 0; _idx < (_name)->n_##_field; _idx++) \
             if ((_node = (_name)->_field[_idx]) != NULL)
+
+/**
+ * Replace a string field in a received message with a different value using the
+ * CMSG memory allocator.
+ * This is useful if the message needs a slight modification before sending on
+ * to another destination so that the memory allocation tracing is kept happy,
+ * and CMSG_FREE_RECV_MSG can still be used to free the whole message.
+ * @param _name name of message ptr variable.
+ * @param _field name of the repeated field
+ * @param _new_value string to be copied to field.
+ */
+#define CMSG_UPDATE_RECV_MSG_STRING_FIELD(_name, _field, _new_value) \
+    cmsg_update_recv_msg_string_field (&((_name)->_field), _new_value, __FILE__, __LINE__)
 
 int cmsg_service_port_get (const char *name, const char *proto);
 

commit c2f485ce88037f082eed523764d214c35026af24

VCSAPI-236: Fix order of variables in CMSG_STRDUP macro
This macro is currently unused.
Fixes: 0e1c853e138514da27011f7c95b9ef087889d351

Change-Id: I75237f86fc136fbd2f89b5b7b42609a54531a9d4
Reviewed-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/cmsg_private.h
@@ -256,7 +256,7 @@ cmsg_tlv_header_process (uint8_t *buf, cmsg_server_request *server_request,
 #define CMSG_CALLOC(nmemb,size)     cmsg_calloc ((nmemb), (size), __FILE__,  __LINE__)
 #define CMSG_ASPRINTF(strp,fmt,...) cmsg_asprintf (__FILE__,  __LINE__, (strp),\
                                                    (fmt), ##__VA_ARGS__)
-#define CMSG_STRDUP(strp) cmsg_strdup (__FILE__,  __LINE__, (strp))
+#define CMSG_STRDUP(strp) cmsg_strdup ((strp), __FILE__,  __LINE__)
 #define CMSG_FREE(ptr)              cmsg_free ((ptr),  __FILE__,  __LINE__)
 void *cmsg_malloc (size_t size, const char *filename, int line);
 void *cmsg_calloc (size_t nmemb, size_t size, const char *filename, int line);