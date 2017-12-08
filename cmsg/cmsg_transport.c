alliedtelesis/cmsg

commit 434112d117808ef158db787b7e37de7ec94a6916

ENGBL-736: Move select() call into specific recv_wrapper functions
We don't want to do the select call for UDTs as they may not pass in
a valid socket. Simply move the existing select call down a layer into
the specific transports for now. Also allow negative socket numbers
through as a UDT may pass one (even though it isn't using it).

Change-Id: I0d30d09b9b8bd0c93be2a98bfd60c098656090a0
Reviewed-by: Brad Carthew <brad.carthew@alliedtelesis.co.nz>

cmsg/cmsg_transport.c
@@ -235,18 +235,6 @@ cmsg_transport_peek_for_header (cmsg_recv_func recv_wrapper, cmsg_transport *tra
     int count = 0;
     int nbytes = 0;
 
-    struct timeval timeout = { 1, 0 };
-    fd_set read_fds;
-    int maxfd;
-
-    FD_ZERO (&read_fds);
-    FD_SET (socket, &read_fds);
-    maxfd = socket;
-
-    /* Do select() on the socket to prevent it to go to usleep instantaneously in the loop
-     * if the data is not yet available.*/
-    select (maxfd + 1, &read_fds, NULL, NULL, &timeout);
-
     /* Peek until data arrives, this allows us to timeout and recover if no data arrives. */
     while ((count < maxLoop) && (nbytes != (int) sizeof (cmsg_header)))
     {
@@ -661,33 +649,25 @@ cmsg_transport_server_recv (int32_t server_socket, cmsg_server *server)
     cmsg_header header_received;
     cmsg_transport *transport = NULL;
 
-    if (!server || server_socket < 0)
+    CMSG_ASSERT_RETURN_VAL (server != NULL, CMSG_RET_ERR);
+
+    transport = server->_transport;
+
+    /* Remember the client socket to use when send reply */
+    transport->connection.sockets.client_socket = server_socket;
+
+    peek_status = cmsg_transport_peek_for_header (transport->tport_funcs.recv_wrapper,
+                                                  transport,
+                                                  server_socket, MAX_SERVER_PEEK_LOOP,
+                                                  &header_received);
+    if (peek_status == CMSG_STATUS_CODE_SUCCESS)
     {
-        CMSG_LOG_TRANSPORT_ERROR (server->_transport,
-                                  "Bad parameter for server recv. Server:%p Socket:%d",
-                                  server, server_socket);
+        ret = _cmsg_transport_server_recv (transport->tport_funcs.recv_wrapper,
+                                           server_socket, server, &header_received);
     }
-    else
+    else if (peek_status == CMSG_STATUS_CODE_CONNECTION_CLOSED)
     {
-        CMSG_DEBUG (CMSG_INFO, "[TRANSPORT] socket %d\n", server_socket);
-
-        transport = server->_transport;
-        /* Remember the client socket to use when send reply */
-        transport->connection.sockets.client_socket = server_socket;
-
-        peek_status = cmsg_transport_peek_for_header (transport->tport_funcs.recv_wrapper,
-                                                      transport,
-                                                      server_socket, MAX_SERVER_PEEK_LOOP,
-                                                      &header_received);
-        if (peek_status == CMSG_STATUS_CODE_SUCCESS)
-        {
-            ret = _cmsg_transport_server_recv (transport->tport_funcs.recv_wrapper,
-                                               server_socket, server, &header_received);
-        }
-        else if (peek_status == CMSG_STATUS_CODE_CONNECTION_CLOSED)
-        {
-            ret = CMSG_RET_CLOSED;
-        }
+        ret = CMSG_RET_CLOSED;
     }
 
     return ret;

cmsg/cmsg_transport_tcp.c
@@ -208,6 +208,18 @@ int
 cmsg_transport_tcp_recv (cmsg_transport *transport, int sock, void *buff, int len,
                          int flags)
 {
+    struct timeval timeout = { 1, 0 };
+    fd_set read_fds;
+    int maxfd;
+
+    FD_ZERO (&read_fds);
+    FD_SET (sock, &read_fds);
+    maxfd = sock;
+
+    /* Do select() on the socket to prevent it to go to usleep instantaneously in the loop
+     * if the data is not yet available.*/
+    select (maxfd + 1, &read_fds, NULL, NULL, &timeout);
+
     return recv_all (sock, buff, len, flags);
 }
 

cmsg/cmsg_transport_tipc.c
@@ -168,6 +168,18 @@ int
 cmsg_transport_tipc_recv (cmsg_transport *transport, int sock, void *buff, int len,
                           int flags)
 {
+    struct timeval timeout = { 1, 0 };
+    fd_set read_fds;
+    int maxfd;
+
+    FD_ZERO (&read_fds);
+    FD_SET (sock, &read_fds);
+    maxfd = sock;
+
+    /* Do select() on the socket to prevent it to go to usleep instantaneously in the loop
+     * if the data is not yet available.*/
+    select (maxfd + 1, &read_fds, NULL, NULL, &timeout);
+
     return recv (sock, buff, len, flags);
 }
 

cmsg/cmsg_transport_tipc_broadcast.c
@@ -80,6 +80,17 @@ cmsg_transport_tipc_broadcast_recv (cmsg_transport *transport, int sock, void *b
 {
     uint32_t addrlen = 0;
     int nbytes;
+    struct timeval timeout = { 1, 0 };
+    fd_set read_fds;
+    int maxfd;
+
+    FD_ZERO (&read_fds);
+    FD_SET (sock, &read_fds);
+    maxfd = sock;
+
+    /* Do select() on the socket to prevent it to go to usleep instantaneously in the loop
+     * if the data is not yet available.*/
+    select (maxfd + 1, &read_fds, NULL, NULL, &timeout);
 
     addrlen = sizeof (struct sockaddr_tipc);
 

cmsg/cmsg_transport_unix.c
@@ -119,6 +119,18 @@ int
 cmsg_transport_unix_recv (cmsg_transport *transport, int sock, void *buff, int len,
                           int flags)
 {
+    struct timeval timeout = { 1, 0 };
+    fd_set read_fds;
+    int maxfd;
+
+    FD_ZERO (&read_fds);
+    FD_SET (sock, &read_fds);
+    maxfd = sock;
+
+    /* Do select() on the socket to prevent it to go to usleep instantaneously in the loop
+     * if the data is not yet available.*/
+    select (maxfd + 1, &read_fds, NULL, NULL, &timeout);
+
     return recv (sock, buff, len, flags);
 }
 

commit c198e6777ea774cba9efd9d3341f5f752a0542ab

ENGBL-906: Calculate time taken to peek the header in seconds
A previous patch moved a single select call with a timeout value of 1 second
down into the transport recv_wrapper functions. This inadvertently meant we
could spend up to 100000 seconds waiting to peek the header. If the other
end never sends a response (i.e. the stack member has left) then eventually
the device will healthcheck before 100000 seconds is hit.

Note that 'gettimeofday' of day is used rather than 'time' and 'difftime'
(which would have produced slightly cleaner/simpler code) to avoid the
use of expensive floating point calculations.

fixes: commit 434112d ("ENGBL-736: Move select() call into specific recv_wrapper functions")

Change-Id: Ib708b280428cf2231b66b8d79a489026c3d60df9
Reviewed-by: Brad Carthew <brad.carthew@alliedtelesis.co.nz>

cmsg/cmsg_transport.c
@@ -222,20 +222,34 @@ cmsg_transport_destroy (cmsg_transport *transport)
     }
 }
 
-/* Poll for the header data, give up if we timeout. This is used to avoid blocking forever
- * on the receive if the data is never sent or is partially sent.
+/**
+ * Poll for the header data and give up if we timeout. This is used to avoid
+ * blocking forever on the receive if the data is never sent or is partially sent.
+ *
+ * @param recv_wrapper - The transport specific receive function to use with a socket.
+ * @param transport - The transport that is doing the peeking.
+ * @param socket - The socket to peek the data off.
+ * @param seconds_to_wait - The number of seconds to wait before timing out and giving up.
+ * @param header_received - Pointer to a header structure to return the peeked header in.
+ *
+ * @returns CMSG_STATUS_CODE_SUCCESS on success, the related cmsg_status_code otherwise.
  */
 cmsg_status_code
 cmsg_transport_peek_for_header (cmsg_recv_func recv_wrapper, cmsg_transport *transport,
-                                int32_t socket, int32_t maxLoop,
+                                int32_t socket, time_t seconds_to_wait,
                                 cmsg_header *header_received)
 {
     cmsg_status_code ret = CMSG_STATUS_CODE_SUCCESS;
-    int count = 0;
     int nbytes = 0;
+    bool timed_out = false;
+    time_t seconds_waited = 0;
+    struct timeval start;
+    struct timeval current;
 
-    /* Peek until data arrives, this allows us to timeout and recover if no data arrives. */
-    while ((count < maxLoop) && (nbytes != (int) sizeof (cmsg_header)))
+    gettimeofday (&start, NULL);
+
+    /* Peek until data arrives. This allows us to timeout and recover if no data arrives. */
+    while (!timed_out)
     {
         nbytes = recv_wrapper (transport, socket, header_received,
                                sizeof (cmsg_header), MSG_PEEK | MSG_DONTWAIT);
@@ -278,11 +292,23 @@ cmsg_transport_peek_for_header (cmsg_recv_func recv_wrapper, cmsg_transport *tra
                 }
             }
         }
-        usleep (1000);
-        count++;
+
+        gettimeofday (&current, NULL);
+        seconds_waited = current.tv_sec - start.tv_sec;
+        if (seconds_waited > seconds_to_wait)
+        {
+            timed_out = true;
+        }
+        else
+        {
+            /* The recv_wrapper function may not implement any blocking
+             * (e.g. a select call). Therefore do a small sleep here to
+             * avoid continuously keeping the CPU busy. */
+            usleep (1000);
+        }
     }
 
-    if (count >= maxLoop)
+    if (timed_out)
     {
         // Report the failure and try to recover
         CMSG_LOG_TRANSPORT_ERROR (transport,
@@ -291,10 +317,11 @@ cmsg_transport_peek_for_header (cmsg_recv_func recv_wrapper, cmsg_transport *tra
 
         ret = CMSG_STATUS_CODE_SERVICE_FAILED;
     }
-    else if (count >= maxLoop / 2)
+    else if (seconds_waited >= seconds_to_wait / 2)
     {
         // This should not really happen, log it
-        CMSG_LOG_TRANSPORT_ERROR (transport, "Receive looped %d times", count);
+        CMSG_LOG_TRANSPORT_ERROR (transport, "Receive took %u seconds",
+                                  (uint32_t) seconds_waited);
     }
 
     return ret;

cmsg/cmsg_transport.h
@@ -30,10 +30,10 @@
 #define CONNECT_TIMEOUT_DEFAULT -1
 
 /* This value is used to limit the timeout for client message peek to 100s */
-#define MAX_CLIENT_PEEK_LOOP (100000)
+#define MAX_CLIENT_PEEK_LOOP (100)
 
 /* This value is used to limit the timeout for server message peek to 10s */
-#define MAX_SERVER_PEEK_LOOP (10000)
+#define MAX_SERVER_PEEK_LOOP (10)
 
 #ifdef HAVE_VCSTACK
 typedef struct _cpg_connection_s
@@ -281,8 +281,8 @@ char *cmsg_transport_unix_sun_path (const ProtobufCServiceDescriptor *descriptor
 void cmsg_transport_unix_sun_path_free (char *sun_path);
 
 cmsg_status_code cmsg_transport_peek_for_header (cmsg_recv_func recv_wrapper,
-                                                 cmsg_transport *transport,
-                                                 int32_t socket, int32_t maxLoop,
+                                                 cmsg_transport *transport, int32_t socket,
+                                                 time_t seconds_to_wait,
                                                  cmsg_header *header_received);
 
 const char *cmsg_transport_counter_app_tport_id (cmsg_transport *transport);