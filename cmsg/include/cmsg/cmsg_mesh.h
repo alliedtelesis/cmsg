d3c5d595ac72c8c4abba737ffa7ba1270fcedfcb
Matt Bennett <matt.bennett@alliedtelesis.co.nz>
2018-02-16 08:33:40 +1300

    ENGBL-647: Create CMSG 'mesh' concept
    This abstracts a broadcast client, loopback client and server into a
    single structure. This can then be used to further abstract the numerous
    places in AW+ that we use the above types to implement broadcasting
    configuration across the stack.
    
    Change-Id: If202028ca05251f4d5bb2b9a4b5321f7e1dde558
    Reviewed-by: Brad Carthew <brad.carthew@alliedtelesis.co.nz>
    Reviewed-by: John Thompson <john.thompson@alliedtelesis.co.nz>

---
+++ cmsg/include/cmsg/cmsg_mesh.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018, Allied Telesis Labs New Zealand, Ltd
+ */
+#ifndef __CMSG_MESH_H_
+#define __CMSG_MESH_H_
+
+#include "cmsg_client.h"
+#include "cmsg_server.h"
+
+typedef struct _cmsg_tipc_mesh_conn
+{
+    cmsg_server *server;
+    cmsg_server_accept_thread_info *server_thread_info;
+    cmsg_client *broadcast_client;
+    cmsg_client *loopback_client;
+} cmsg_tipc_mesh_conn;
+
+/**
+* Type of CMSG mesh connection to use.
+*
+* CMSG_MESH_LOCAL_NONE - Messages are not sent back to the sending node
+* CMSG_MESH_LOCAL_LOOPBACK - Messages are sent back to the sending node via
+*                            a loopback client (i.e. in the same thread that is sending).
+* CMSG_MESH_LOCAL_TIPC - Messages are sent back to the sending node via a
+*                        TIPC client. This assumes the required TIPC server is running
+*                        in a separate thread to the one that is sending.
+*/
+typedef enum _cmsg_mesh_local_e
+{
+    CMSG_MESH_LOCAL_NONE,
+    CMSG_MESH_LOCAL_LOOPBACK,
+    CMSG_MESH_LOCAL_TIPC,
+} cmsg_mesh_local_type;
+
+cmsg_tipc_mesh_conn *cmsg_tipc_mesh_connection_init (ProtobufCService *service,
+                                                     const char *service_entry_name,
+                                                     uint32_t my_node_id,
+                                                     uint32_t lower_node_id,
+                                                     uint32_t upper_node_id,
+                                                     cmsg_mesh_local_type type,
+                                                     bool oneway);
+
+#endif /* __CMSG_MESH_H_ */
---
+++ cmsg/src/cmsg_mesh.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2018, Allied Telesis Labs New Zealand, Ltd
+ */
+
+#include "cmsg_mesh.h"
+#include "cmsg_broadcast_client.h"
+
+cmsg_tipc_mesh_conn *
+cmsg_tipc_mesh_connection_init (ProtobufCService *service, const char *service_entry_name,
+                                uint32_t my_node_id, uint32_t lower_node_id,
+                                uint32_t upper_node_id, cmsg_mesh_local_type type,
+                                bool oneway)
+{
+    cmsg_tipc_mesh_conn *mesh_info = NULL;
+    cmsg_client *bcast_client = NULL;
+    cmsg_client *loopback_client = NULL;
+    cmsg_server *server = NULL;
+    cmsg_server_accept_thread_info *server_thread_info = NULL;
+    bool connect_to_self = (type == CMSG_MESH_LOCAL_TIPC);
+    bool create_loopback = (type == CMSG_MESH_LOCAL_LOOPBACK);
+    int ret = -1;
+
+    mesh_info = CMSG_CALLOC (1, sizeof (cmsg_tipc_mesh_conn));
+    if (mesh_info == NULL)
+    {
+        return NULL;
+    }
+
+    bcast_client = cmsg_broadcast_client_new (service->descriptor,
+                                              service_entry_name, my_node_id,
+                                              lower_node_id, upper_node_id,
+                                              connect_to_self, oneway);
+
+    if (bcast_client == NULL)
+    {
+        CMSG_FREE (mesh_info);
+        return NULL;
+    }
+
+    mesh_info->broadcast_client = bcast_client;
+
+    if (create_loopback)
+    {
+        loopback_client = cmsg_create_client_loopback (service);
+        if (loopback_client == NULL)
+        {
+            cmsg_broadcast_client_destroy (mesh_info->broadcast_client);
+            CMSG_FREE (mesh_info);
+            return NULL;
+        }
+
+        mesh_info->loopback_client = loopback_client;
+        ret = cmsg_broadcast_client_add_loopback (mesh_info->broadcast_client,
+                                                  loopback_client);
+        if (ret != CMSG_RET_OK)
+        {
+            cmsg_destroy_client_and_transport (mesh_info->loopback_client);
+            cmsg_broadcast_client_destroy (mesh_info->broadcast_client);
+            CMSG_FREE (mesh_info);
+            return NULL;
+        }
+    }
+
+    server = cmsg_create_server_tipc_rpc (service_entry_name, my_node_id,
+                                          TIPC_CLUSTER_SCOPE, service);
+
+    if (!server)
+    {
+        cmsg_destroy_client_and_transport (mesh_info->loopback_client);
+        cmsg_broadcast_client_destroy (mesh_info->broadcast_client);
+        CMSG_FREE (mesh_info);
+        return NULL;
+    }
+    mesh_info->server = server;
+
+    server_thread_info = cmsg_server_accept_thread_init (mesh_info->server);
+    if (server_thread_info == NULL)
+    {
+        cmsg_destroy_server_and_transport (mesh_info->server);
+        cmsg_destroy_client_and_transport (mesh_info->loopback_client);
+        cmsg_broadcast_client_destroy (mesh_info->broadcast_client);
+        CMSG_FREE (mesh_info);
+        return NULL;
+    }
+    mesh_info->server_thread_info = server_thread_info;
+
+    return mesh_info;
+}
