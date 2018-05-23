ba3a686c1169094c120538a79929640fd50278e0
Matt Bennett <matt.bennett@alliedtelesis.co.nz>
2018-05-23 16:48:05 +1200

    VISTA548-224: Use separate RPC to close streaming connection
    This simplifies the code for users that call the API as they do not
    need data to send when closing the connection.
    
    Change-Id: I37a5d92608c6d8b3d78b6e55e91f3d44f5f6a3a8
    Reviewed-by: Luuk Paulussen <luuk.paulussen@alliedtelesis.co.nz>
    Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

--- cmsg-proxy/src/cmsg_proxy_http_streaming.c
+++ cmsg-proxy/src/cmsg_proxy_http_streaming.c
@@ -484,17 +484,34 @@ http_streaming_impl_send_stream_data (const void *service, const stream_data *re
 
     stream_response_send (data);
 
-    if (recv_msg->status == STREAM_STATUS_CLOSE)
-    {
-        stream_response_close (connection_info->connection);
-        pthread_mutex_lock (&stream_connections_mutex);
-        stream_connections_list = g_list_remove (stream_connections_list, connection_info);
-        pthread_mutex_unlock (&stream_connections_mutex);
-        connection_info->to_delete = true;
-        connection_info->in_use = false;
-    }
-
     cmsg_proxy_streaming_release_conn_info (connection_info);
 
     http_streaming_server_send_stream_dataSend (service, &send_msg);
+}
+
+void
+http_streaming_impl_close_stream_connection (const void *service, const stream_id *recv_msg)
+{
+    server_response send_msg = SERVER_RESPONSE_INIT;
+    cmsg_proxy_stream_connection *connection_info = NULL;
+    bool found = false;
+
+    connection_info = cmsg_proxy_streaming_lookup_conn_by_id (recv_msg->id);
+    if (connection_info)
+    {
+        stream_response_close (connection_info->connection);
+
+        pthread_mutex_lock (&stream_connections_mutex);
+        stream_connections_list = g_list_remove (stream_connections_list, connection_info);
+        pthread_mutex_unlock (&stream_connections_mutex);
+
+        connection_info->to_delete = true;
+        connection_info->in_use = false;
+        cmsg_proxy_streaming_release_conn_info (connection_info);
+        found = true;
+    }
+
+    CMSG_SET_FIELD_VALUE (&send_msg, stream_found, found);
+
+    http_streaming_server_close_stream_connectionSend (service, &send_msg);
 }
