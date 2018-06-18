alliedtelesis/cmsg

commit 8b16d24a7fd4990e4f13e003006622b139fa9d04

ENGBL-1037: Update broadcast client to generate events on node join/leave
The CMSG broadcast client / mesh framework is intended as a replacement for
'closed process group' (CPG) communication from corosync. CPG generates events
as nodes join/leave the group and places in AW+ using CPG make use of these
events. Therefore, add similar functionality to CMSG so that the existing
code using CPG can simply be replaced with CMSG.

Change-Id: I27a15c407b993d5892b71a254bc998dd98da6ab9
Reviewed-by: Luuk Paulussen <luuk.paulussen@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/src/broadcast_client/cmsg_broadcast_client_conn_mgmt.c
@@ -4,11 +4,36 @@
  * Copyright 2017, Allied Telesis Labs New Zealand, Ltd
  */
 
+#define _GNU_SOURCE /* for TEMP_FAILURE_RETRY */
+#include <sys/eventfd.h>
 #include <glib.h>
 #include "cmsg_broadcast_client_private.h"
 #include "cmsg_transport.h"
 #include "cmsg_error.h"
 #include "cmsg_composite_client.h"
+
+/**
+ * Generate an event for the node/leave join.
+ *
+ * @param broadcast_client - The broadcast client to queue the event on.
+ * @param node_id - The id of the node that has joined/left.
+ * @param joined - true if the node has joined, false if it has left.
+ */
+static void
+cmsg_broadcast_client_generate_event (cmsg_broadcast_client *broadcast_client,
+                                      uint32_t node_id, bool joined)
+{
+    cmsg_broadcast_client_event *event = NULL;
+
+    event = CMSG_MALLOC (sizeof (cmsg_broadcast_client_event));
+    if (event)
+    {
+        event->node_id = node_id;
+        event->joined = joined;
+        g_async_queue_push (broadcast_client->event_queue.queue, event);
+        TEMP_FAILURE_RETRY (eventfd_write (broadcast_client->event_queue.eventfd, 1));
+    }
+}
 
 /**
  * Create a TIPC client to the given node id, connect the client,
@@ -62,6 +87,8 @@ cmsg_broadcast_client_add_child (cmsg_broadcast_client *broadcast_client,
     }
 
     cmsg_composite_client_add_child (comp_client, child);
+
+    cmsg_broadcast_client_generate_event (broadcast_client, tipc_node_id, true);
 }
 
 /**
@@ -105,6 +132,8 @@ cmsg_broadcast_client_delete_child (cmsg_broadcast_client *broadcast_client,
     }
 
     cmsg_destroy_client_and_transport (child);
+
+    cmsg_broadcast_client_generate_event (broadcast_client, tipc_node_id, false);
 }
 
 /**

cmsg/include/cmsg/cmsg_broadcast_client.h
@@ -10,14 +10,20 @@
 #include "cmsg.h"
 #include "cmsg_client.h"
 
+typedef void (*cmsg_broadcast_event_handler_t) (uint32_t node_id, bool joined);
+
 cmsg_client *cmsg_broadcast_client_new (const ProtobufCServiceDescriptor *descriptor,
                                         const char *service_entry_name,
                                         uint32_t my_node_id, uint32_t lower_node_id,
                                         uint32_t upper_node_id,
-                                        bool connect_to_self, bool oneway);
+                                        bool connect_to_self, bool oneway,
+                                        cmsg_broadcast_event_handler_t event_handler);
 void cmsg_broadcast_client_destroy (cmsg_client *client);
 
 int32_t cmsg_broadcast_client_add_loopback (cmsg_client *broadcast_client,
                                             cmsg_client *loopback_client);
+
+int cmsg_broadcast_client_get_event_fd (cmsg_client *_broadcast_client);
+void cmsg_broadcast_event_queue_process (cmsg_client *_broadcast_client);
 
 #endif /* __CMSG_BROADCAST_CLIENT_H_ */

cmsg/include/cmsg/cmsg_mesh.h
@@ -6,6 +6,7 @@
 
 #include "cmsg_client.h"
 #include "cmsg_server.h"
+#include "cmsg_broadcast_client.h"
 
 typedef struct _cmsg_tipc_mesh_conn
 {
@@ -38,6 +39,8 @@ cmsg_tipc_mesh_conn *cmsg_tipc_mesh_connection_init (ProtobufCService *service,
                                                      uint32_t lower_node_id,
                                                      uint32_t upper_node_id,
                                                      cmsg_mesh_local_type type,
-                                                     bool oneway);
+                                                     bool oneway,
+                                                     cmsg_broadcast_event_handler_t
+                                                     event_handler);
 
 #endif /* __CMSG_MESH_H_ */

cmsg/src/broadcast_client/cmsg_broadcast_client.c
@@ -4,11 +4,31 @@
  * Copyright 2017, Allied Telesis Labs New Zealand, Ltd
  */
 
+#define _GNU_SOURCE /* for TEMP_FAILURE_RETRY */
+#include <sys/eventfd.h>
 #include "cmsg_broadcast_client_private.h"
 #include "cmsg_broadcast_client.h"
 #include "cmsg_error.h"
 #include "cmsg_composite_client.h"
 
+/**
+ * When destroying the broadcast client event queue there may still be
+ * events on there. Simply free them as required to avoid leaking memory.
+ */
+static void
+_clear_event_queue (gpointer data)
+{
+    free (data);
+}
+
+/**
+ * Create a broadcast client structure. Simply allocate the required memory
+ * and initialise all fields to their default values.
+ *
+ * @param descriptor - The service descriptor the broadcast client is for.
+ *
+ * @returns A pointer to the created client on success. NULL otherwise.
+ */
 static cmsg_broadcast_client *
 cmsg_broadcast_client_create (const ProtobufCServiceDescriptor *descriptor)
 {
@@ -32,6 +52,9 @@ cmsg_broadcast_client_create (const ProtobufCServiceDescriptor *descriptor)
         broadcast_client->upper_node_id = 0;
         broadcast_client->tipc_subscription_sd = -1;
         broadcast_client->connect_to_self = false;
+        broadcast_client->event_queue.queue = NULL;
+        broadcast_client->event_queue.eventfd = -1;
+        broadcast_client->event_queue.handler = NULL;
     }
     else
     {
@@ -39,6 +62,58 @@ cmsg_broadcast_client_create (const ProtobufCServiceDescriptor *descriptor)
     }
 
     return broadcast_client;
+}
+
+/**
+ * Deinitialise the event handling functionality for the given broadcast client.
+ *
+ * @param broadcast_client - The broadcast client to deinitialise event handling for.
+ */
+static void
+cmsg_broadcast_client_deinit_events (cmsg_broadcast_client *broadcast_client)
+{
+    broadcast_client->event_queue.handler = NULL;
+
+    if (broadcast_client->event_queue.eventfd >= 0)
+    {
+        close (broadcast_client->event_queue.eventfd);
+        broadcast_client->event_queue.eventfd = -1;
+    }
+    if (broadcast_client->event_queue.queue)
+    {
+        g_async_queue_unref (broadcast_client->event_queue.queue);
+        broadcast_client->event_queue.queue = NULL;
+    }
+}
+
+/**
+ * Initialise the event handling functionality for the given broadcast client.
+ *
+ * @param broadcast_client - The broadcast client to initialise event handling for.
+ * @param event_handler - The function to call for each event that occurs.
+ *
+ * @returns true if initialisation is successful, false otherwise.
+ */
+static bool
+cmsg_broadcast_client_init_events (cmsg_broadcast_client *broadcast_client,
+                                   cmsg_broadcast_event_handler_t event_handler)
+{
+    broadcast_client->event_queue.eventfd = eventfd (0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if (broadcast_client->event_queue.eventfd < 0)
+    {
+        return false;
+    }
+
+    broadcast_client->event_queue.queue = g_async_queue_new_full (_clear_event_queue);
+    if (!broadcast_client->event_queue.queue)
+    {
+        close (broadcast_client->event_queue.eventfd);
+        return false;
+    }
+
+    broadcast_client->event_queue.handler = event_handler;
+
+    return true;
 }
 
 /**
@@ -53,7 +128,9 @@ cmsg_broadcast_client_create (const ProtobufCServiceDescriptor *descriptor)
  * @param upper_node_id - The highest TIPC ID we are interest in broadcasting to.
  *                        (STK_NODEID_MAX)
  * @param connect_to_self - Whether to connect to a server running locally on this node.
- * @param one_way - Whether to do one way broadcasting (true), or RPC broadcasting (false).
+ * @param oneway - Whether to do one way broadcasting (true), or RPC broadcasting (false).
+ * @param event_handler - Function to call on node join/leave events for the broadcast client,
+ *                        if NULL is given then no events will be generated.
  *
  * @return pointer to the client on success, NULL otherwise.
  */
@@ -61,7 +138,8 @@ cmsg_client *
 cmsg_broadcast_client_new (const ProtobufCServiceDescriptor *descriptor,
                            const char *service_entry_name,
                            uint32_t my_node_id, uint32_t lower_node_id,
-                           uint32_t upper_node_id, bool connect_to_self, bool oneway)
+                           uint32_t upper_node_id, bool connect_to_self, bool oneway,
+                           cmsg_broadcast_event_handler_t event_handler)
 {
     int ret = CMSG_RET_OK;
     cmsg_broadcast_client *broadcast_client = NULL;
@@ -82,9 +160,20 @@ cmsg_broadcast_client_new (const ProtobufCServiceDescriptor *descriptor,
     broadcast_client->upper_node_id = upper_node_id;
     broadcast_client->connect_to_self = connect_to_self;
 
+    if (event_handler)
+    {
+        if (!cmsg_broadcast_client_init_events (broadcast_client, event_handler))
+        {
+            cmsg_composite_client_deinit (&broadcast_client->base_client);
+            CMSG_FREE (broadcast_client);
+            return NULL;
+        }
+    }
+
     ret = cmsg_broadcast_conn_mgmt_init (broadcast_client);
     if (ret != CMSG_RET_OK)
     {
+        cmsg_broadcast_client_deinit_events (broadcast_client);
         cmsg_composite_client_deinit (&broadcast_client->base_client);
         CMSG_FREE (broadcast_client);
         return NULL;
@@ -95,6 +184,8 @@ cmsg_broadcast_client_new (const ProtobufCServiceDescriptor *descriptor,
 
 /**
  * Destroy the broadcast client.
+ *
+ * @param client - The broadcast client to destroy.
  */
 void
 cmsg_broadcast_client_destroy (cmsg_client *client)
@@ -117,11 +208,18 @@ cmsg_broadcast_client_destroy (cmsg_client *client)
 
     cmsg_composite_client_deinit (&broadcast_client->base_client);
 
+    cmsg_broadcast_client_deinit_events (broadcast_client);
+
     CMSG_FREE (broadcast_client);
 }
 
 /**
  * Add a loopback client to a broadcast client.
+ *
+ * @param _broadcast_client - The broadcast client to add a loopback client to.
+ * @param loopback_client - The loopback client to add.
+ *
+ * @returns CMSG_RET_OK on success, CMSG_RET_ERR on failure.
  */
 int32_t
 cmsg_broadcast_client_add_loopback (cmsg_client *_broadcast_client,
@@ -136,4 +234,45 @@ cmsg_broadcast_client_add_loopback (cmsg_client *_broadcast_client,
     cmsg_client *comp_client = (cmsg_client *) &broadcast_client->base_client;
 
     return cmsg_composite_client_add_child (comp_client, loopback_client);
+}
+
+/**
+ * Get the eventfd descriptor for the event queue of the given broadcast client
+ *
+ * @param _broadcast_client - The broadcast client to get the eventfd descriptor for.
+ *
+ * @returns The eventfd descriptor on success, -1 on failure.
+ */
+int
+cmsg_broadcast_client_get_event_fd (cmsg_client *_broadcast_client)
+{
+    cmsg_broadcast_client *broadcast_client = (cmsg_broadcast_client *) _broadcast_client;
+
+    CMSG_ASSERT_RETURN_VAL (broadcast_client != NULL, -1);
+
+    return broadcast_client->event_queue.eventfd;
+}
+
+/**
+ * Process any events on the event queue of the given broadcast client.
+ *
+ * @param _broadcast_client - The broadcast client to process events for.
+ */
+void
+cmsg_broadcast_event_queue_process (cmsg_client *_broadcast_client)
+{
+    cmsg_broadcast_client *broadcast_client = (cmsg_broadcast_client *) _broadcast_client;
+    cmsg_broadcast_client_event *event = NULL;
+    eventfd_t value;
+
+    CMSG_ASSERT_RETURN_VOID (broadcast_client != NULL);
+
+    /* clear notification */
+    TEMP_FAILURE_RETRY (eventfd_read (broadcast_client->event_queue.eventfd, &value));
+
+    while ((event = g_async_queue_try_pop (broadcast_client->event_queue.queue)))
+    {
+        broadcast_client->event_queue.handler (event->node_id, event->joined);
+        CMSG_FREE (event);
+    }
 }

cmsg/src/broadcast_client/cmsg_broadcast_client_private.h
@@ -11,6 +11,30 @@
 #include "../cmsg_composite_client_private.h"
 #include "cmsg_broadcast_client.h"
 
+typedef struct _cmsg_broadcast_client_event
+{
+    /* Node ID of the node that has joined/left the broadcast client. */
+    uint32_t node_id;
+
+    /* true if the given node has joined the broadcast client, false if
+     * it has left. */
+    bool joined;
+} cmsg_broadcast_client_event;
+
+typedef struct _cmsg_broadcast_client_event_queue
+{
+    /* Queue to store node join/leave events. This is used to
+     * pass the new socket descriptors back to the server user. */
+    GAsyncQueue *queue;
+
+    /* An eventfd object to notify the listener that there is a new
+     * event on the queue.  */
+    int eventfd;
+
+    /* Function to call on each event */
+    cmsg_broadcast_event_handler_t handler;
+} cmsg_broadcast_client_event_queue;
+
 typedef struct _cmsg_broadcast_client_s
 {
     cmsg_composite_client base_client;
@@ -36,6 +60,9 @@ typedef struct _cmsg_broadcast_client_s
 
     /* Thread for monitoring the TIPC topology and creating clients as required */
     pthread_t topology_thread;
+
+    /* Queue for storing node join/leave events to the broadcast client */
+    cmsg_broadcast_client_event_queue event_queue;
 } cmsg_broadcast_client;
 
 int32_t cmsg_broadcast_conn_mgmt_init (cmsg_broadcast_client *broadcast_client);

cmsg/src/cmsg_glib_helpers.c
@@ -124,7 +124,7 @@ cmsg_glib_tipc_mesh_init (ProtobufCService *service, const char *service_entry_n
     cmsg_tipc_mesh_conn *mesh =
         cmsg_tipc_mesh_connection_init (service, service_entry_name, this_node_id,
                                         min_node_id, max_node_id,
-                                        CMSG_MESH_LOCAL_LOOPBACK, false);
+                                        CMSG_MESH_LOCAL_LOOPBACK, false, NULL);
     if (mesh == NULL)
     {
         CMSG_LOG_GEN_ERROR ("Failed to create mesh connection for %s",

cmsg/src/cmsg_mesh.c
@@ -9,7 +9,7 @@ cmsg_tipc_mesh_conn *
 cmsg_tipc_mesh_connection_init (ProtobufCService *service, const char *service_entry_name,
                                 uint32_t my_node_id, uint32_t lower_node_id,
                                 uint32_t upper_node_id, cmsg_mesh_local_type type,
-                                bool oneway)
+                                bool oneway, cmsg_broadcast_event_handler_t event_handler)
 {
     cmsg_tipc_mesh_conn *mesh_info = NULL;
     cmsg_client *bcast_client = NULL;
@@ -29,7 +29,7 @@ cmsg_tipc_mesh_connection_init (ProtobufCService *service, const char *service_e
     bcast_client = cmsg_broadcast_client_new (service->descriptor,
                                               service_entry_name, my_node_id,
                                               lower_node_id, upper_node_id,
-                                              connect_to_self, oneway);
+                                              connect_to_self, oneway, event_handler);
 
     if (bcast_client == NULL)
     {

commit c95149222324573f977d16a69b94d3a5c16354cb

ENGBL-1037: Ensure novaprova tests compile and pass
The previous patch added a new parameter to the cmsg_broadcast_client_new function
but did not update the existing usages of it in the tests. Also the event generation
code is incorrect. We only want to generate events if the event queue has been
initialised.

fixes: commit 8b16d24 ("ENGBL-1037: Update broadcast client to generate events on node join/leave")

Change-Id: I413ab30c5634aa13c6f3af5cd348dfe7e22ff18c
Reviewed-by: Luuk Paulussen <luuk.paulussen@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/src/broadcast_client/cmsg_broadcast_client_conn_mgmt.c
@@ -25,13 +25,16 @@ cmsg_broadcast_client_generate_event (cmsg_broadcast_client *broadcast_client,
 {
     cmsg_broadcast_client_event *event = NULL;
 
-    event = CMSG_MALLOC (sizeof (cmsg_broadcast_client_event));
-    if (event)
+    if (broadcast_client->event_queue.queue)
     {
-        event->node_id = node_id;
-        event->joined = joined;
-        g_async_queue_push (broadcast_client->event_queue.queue, event);
-        TEMP_FAILURE_RETRY (eventfd_write (broadcast_client->event_queue.eventfd, 1));
+        event = CMSG_MALLOC (sizeof (cmsg_broadcast_client_event));
+        if (event)
+        {
+            event->node_id = node_id;
+            event->joined = joined;
+            g_async_queue_push (broadcast_client->event_queue.queue, event);
+            TEMP_FAILURE_RETRY (eventfd_write (broadcast_client->event_queue.eventfd, 1));
+        }
     }
 }
 

cmsg/test/functional/broadcast_client_tests.c
@@ -127,7 +127,7 @@ test_broadcast_client__servers_up_after_client_init (void)
 
     broadcast_client = cmsg_broadcast_client_new (CMSG_DESCRIPTOR (cmsg, test), "cmsg-test",
                                                   TEST_CLIENT_TIPC_ID, MIN_TIPC_ID,
-                                                  MAX_TIPC_ID, false, true);
+                                                  MAX_TIPC_ID, false, true, NULL);
 
     NP_ASSERT_NOT_NULL (broadcast_client);
 
@@ -169,7 +169,7 @@ test_broadcast_client__servers_up_before_client_init (void)
 
     broadcast_client = cmsg_broadcast_client_new (CMSG_DESCRIPTOR (cmsg, test), "cmsg-test",
                                                   TEST_CLIENT_TIPC_ID, MIN_TIPC_ID,
-                                                  MAX_TIPC_ID, false, true);
+                                                  MAX_TIPC_ID, false, true, NULL);
 
     NP_ASSERT_NOT_NULL (broadcast_client);
 