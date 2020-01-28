alliedtelesis/cmsg

commit 5ac84f5a685279a94f05501b82d5d9ec078399d6

TORUSAPI-247: Add repeated integer field iterator
CMSG_REPEATED_FOREACH checks if the pointer value is NULL which is not
appropriate for iterating over integers where the value may be zero.
Simply add another helper macro for this case.

Change-Id: I7438c1f73fdc2b2428fec6dea443ae22d9bc646c
Reviewed-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/include/cmsg/cmsg.h
@@ -222,6 +222,17 @@ extern ProtobufCAllocator cmsg_memory_allocator;
             if ((_node = (_name)->_field[_idx]) != NULL)
 
 /**
+ * Helper macro to iterate over the int32/uint32 in a repeated field of a CMSG message
+ * @param _name name of message ptr variable.
+ * @param _field name of the repeated field
+ * @param _node integer of the type of the repeated field.
+ * @param _idx integer variable to use as loop counter.
+ */
+#define CMSG_REPEATED_FOREACH_INT(_name, _field, _node, _idx)  \
+    if ((_name) && (_name)->_field)                         \
+        for (_idx = 0, _node = (_name)->_field[_idx]; _idx < (_name)->n_##_field; _idx++)
+
+/**
  * Replace a string field in a received message with a different value using the
  * CMSG memory allocator.
  * This is useful if the message needs a slight modification before sending on

commit edb3a2c94a74c01d6eb1ed06672a8633a62316b3

TORUSAPI-247: Fix CMSG_REPEATED_FOREACH_INT and add unit test
The iterator delivered in the previous patch was not iterating correctly.
This has been fixed and a unit test has been added.

Change-Id: Ib64d3f5b3c6618303b94f33945e5228326633a92
Fixes: 5ac84f5a685279a94f05501b82d5d9ec078399d6
Reviewed-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/include/cmsg/cmsg.h
@@ -223,6 +223,8 @@ extern ProtobufCAllocator cmsg_memory_allocator;
 
 /**
  * Helper macro to iterate over the int32/uint32 in a repeated field of a CMSG message
+ * Last || condition is to ensure assignment and allow zero values to work correctly.
+ * Avoids compilation conditions due to "always evaluates to true"
  * @param _name name of message ptr variable.
  * @param _field name of the repeated field
  * @param _node integer of the type of the repeated field.
@@ -230,7 +232,8 @@ extern ProtobufCAllocator cmsg_memory_allocator;
  */
 #define CMSG_REPEATED_FOREACH_INT(_name, _field, _node, _idx)  \
     if ((_name) && (_name)->_field)                         \
-        for (_idx = 0, _node = (_name)->_field[_idx]; _idx < (_name)->n_##_field; _idx++)
+        for (_idx = 0, _node = (_name)->_field[_idx]; _idx < (_name)->n_##_field; _idx++) \
+            if ((_node = (_name)->_field[_idx]) || (_name))
 
 /**
  * Replace a string field in a received message with a different value using the

cmsg/test/unit/cmsg_unit_tests.c
@@ -12,8 +12,8 @@
 #define UINT32_VALUE 123456
 #define STRING_VALUE "This is a test message"
 #define INTERNAL_UINT32_VALUE 987654
-#define ARRAY_SIZE 3
-static uint32_t uint32_array[ARRAY_SIZE] = { 123, 234, 345 };
+#define ARRAY_SIZE 4
+static uint32_t uint32_array[ARRAY_SIZE] = { 123, 234, 0, 345 };
 
 #define TEST_FILE_NAME "/tmp/dump_file"
 
@@ -54,4 +54,27 @@ test_cmsg_dump_msg_to_file (void)
                      INTERNAL_UINT32_VALUE);
 
     CMSG_FREE_RECV_MSG (read_data_msg);
+}
+
+/**
+ * Test that CMSG_REPEATED_FOREACH_INT works as advertised.
+ */
+void
+test_cmsg_repeated_foreach_int (void)
+{
+    cmsg_dump_msg dump_msg = CMSG_DUMP_MSG_INIT;
+    cmsg_dump_msg *dump_msg_ptr = &dump_msg;
+    int i;
+    int loop_counter = 0;
+    uint32_t node;
+
+    CMSG_SET_FIELD_REPEATED (dump_msg_ptr, uint32_array, uint32_array, ARRAY_SIZE);
+
+    CMSG_REPEATED_FOREACH_INT (dump_msg_ptr, uint32_array, node, i)
+    {
+        NP_ASSERT_EQUAL (i, loop_counter);
+        NP_ASSERT_EQUAL (node, dump_msg_ptr->uint32_array[loop_counter]);
+        loop_counter++;
+    }
+    NP_ASSERT_EQUAL (loop_counter, ARRAY_SIZE);
 }