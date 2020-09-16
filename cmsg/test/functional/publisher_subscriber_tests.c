48587abea4dad0d1dafb3d7e3dd506c9cbf2aa79
Matt Bennett <matt.bennett@alliedtelesis.co.nz>
2020-09-17 08:58:45 +1200

    MAINT: Use pthread helper for publisher/subscriber functional test
    This removes a usage of 'cmsg_server_receive_poll'.
    
    Change-Id: I8cabe4f43b82fdf7566c86253a24517699b8d679
    Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>
    Reviewed-by: John Thompson <john.thompson@alliedtelesis.co.nz>
    Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

--- cmsg/test/functional/publisher_subscriber_tests.c
+++ cmsg/test/functional/publisher_subscriber_tests.c
@@ -89,11 +89,10 @@ publish_message (cmsg_publisher *publisher)
 static void
 create_sub_before_pub_and_test (cmsg_transport_type type)
 {
+    pthread_t subscriber_thread;
     cmsg_subscriber *sub = NULL;
     int ret = 0;
     struct in_addr addr;
-    int fd = -1;
-    cmsg_server *sub_server = NULL;
     cmsg_publisher *publisher = NULL;
 
     switch (type)
@@ -115,13 +114,8 @@ create_sub_before_pub_and_test (cmsg_transport_type type)
     ret = cmsg_sub_subscribe_local (sub, "simple_notification_test");
     NP_ASSERT_EQUAL (ret, CMSG_RET_OK);
 
-    fd = cmsg_sub_unix_server_socket_get (sub);
-    sub_server = cmsg_sub_unix_server_get (sub);
-    int fd_max = fd + 1;
-
-    fd_set readfds;
-    FD_ZERO (&readfds);
-    FD_SET (fd, &readfds);
+    NP_ASSERT_TRUE (cmsg_pthread_server_init (&subscriber_thread,
+                                              cmsg_sub_unix_server_get (sub)));
 
     publisher = cmsg_publisher_create (CMSG_DESCRIPTOR (cmsg, test));
     NP_ASSERT_NOT_NULL (publisher);
@@ -130,18 +124,11 @@ create_sub_before_pub_and_test (cmsg_transport_type type)
 
     while (subscriber_run)
     {
-        cmsg_server_receive_poll (sub_server, 1000, &readfds, &fd_max);
+        usleep (1000);
     }
 
-    // Close accepted sockets before destroying subscriber
-    for (fd = 0; fd <= fd_max; fd++)
-    {
-        if (FD_ISSET (fd, &readfds))
-        {
-            close (fd);
-        }
-    }
-
+    pthread_cancel (subscriber_thread);
+    pthread_join (subscriber_thread, NULL);
     cmsg_subscriber_destroy (sub);
     cmsg_publisher_destroy (publisher);
 }
