17fa13590c696e9bd2344a85753a591059a63045
Matt Bennett <matt.bennett@alliedtelesis.co.nz>
2020-09-17 10:37:01 +1200

    MAINT: Use pthread helper for broadcast client functional test
    This removes a usage of 'cmsg_server_receive_poll'. Furthermore, we
    remove one of the tests in this file as it isn't actually testing
    the broadcast client functionality. It is indirectly testing the
    same functionality that the pthread helpers is invoking anyway.
    
    Change-Id: I1b30be4c2a30f4de77e73006d08e488188f99f12
    Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>
    Reviewed-by: John Thompson <john.thompson@alliedtelesis.co.nz>
    Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

--- cmsg/test/functional/broadcast_client_tests.c
+++ cmsg/test/functional/broadcast_client_tests.c
@@ -14,18 +14,16 @@
 #include "cmsg_composite_client.h"
 #include "setup.h"
 
-bool server_threads_run = true;
-
 #define TEST_CLIENT_TIPC_ID 5
 #define MIN_TIPC_ID 1
 #define MAX_TIPC_ID 8
 
 static const uint16_t tipc_scope = TIPC_CLUSTER_SCOPE;
 
+static cmsg_server *server1;
 static pthread_t server_thread1;
+static cmsg_server *server2;
 static pthread_t server_thread2;
-
-static bool impl_function_hit = false;
 
 /**
  * Common functionality to run before each test case.
@@ -51,47 +49,29 @@ tear_down (void)
     return 0;
 }
 
-static void *
-server_test_thread_run (void *_tipc_instance)
+static void
+create_servers (void)
 {
-    cmsg_server *server = NULL;
-    uint32_t tipc_instance = (uintptr_t) _tipc_instance;
-
-    server = cmsg_create_server_tipc_rpc ("cmsg-test", tipc_instance, tipc_scope,
-                                          CMSG_SERVICE (cmsg, test));
-
-    int fd = cmsg_server_get_socket (server);
-    int fd_max = fd + 1;
-
-    fd_set readfds;
-    FD_ZERO (&readfds);
-    FD_SET (fd, &readfds);
-
-    while (server_threads_run)
-    {
-        cmsg_server_receive_poll (server, 1000, &readfds, &fd_max);
-    }
-
-    // Close accepted sockets before destroying server
-    for (fd = 0; fd <= fd_max; fd++)
-    {
-        if (FD_ISSET (fd, &readfds))
-        {
-            close (fd);
-        }
-    }
-
-    cmsg_destroy_server_and_transport (server);
-
-    return 0;
+    server1 = cmsg_create_server_tipc_rpc ("cmsg-test", 1, tipc_scope,
+                                           CMSG_SERVICE (cmsg, test));
+    cmsg_pthread_server_init (&server_thread1, server1);
+    server2 = cmsg_create_server_tipc_rpc ("cmsg-test", 2, tipc_scope,
+                                           CMSG_SERVICE (cmsg, test));
+    cmsg_pthread_server_init (&server_thread2, server2);
 }
 
 static void
 stop_servers_and_wait (void)
 {
-    server_threads_run = false;
+    pthread_cancel (server_thread1);
     pthread_join (server_thread1, NULL);
+    cmsg_destroy_server_and_transport (server1);
+    server1 = NULL;
+    pthread_cancel (server_thread2);
     pthread_join (server_thread2, NULL);
+    cmsg_destroy_server_and_transport (server2);
+    server2 = NULL;
+
 }
 
 /**
@@ -103,7 +83,6 @@ void
 test_broadcast_client__servers_up_after_client_init (void)
 {
     cmsg_client *broadcast_client = NULL;
-    uintptr_t cast_id = 0;
 
     broadcast_client = cmsg_broadcast_client_new (CMSG_DESCRIPTOR (cmsg, test), "cmsg-test",
                                                   TEST_CLIENT_TIPC_ID, MIN_TIPC_ID,
@@ -113,12 +92,7 @@ test_broadcast_client__servers_up_after_client_init (void)
 
     NP_ASSERT_EQUAL (cmsg_composite_client_num_children (broadcast_client), 0);
 
-    cast_id = (uintptr_t) 1;
-    pthread_create (&server_thread1, NULL, &server_test_thread_run, (void *) cast_id);
-
-    cast_id = (uintptr_t) 2;
-    pthread_create (&server_thread2, NULL, &server_test_thread_run, (void *) cast_id);
-
+    create_servers ();
     sleep (2);
 
     NP_ASSERT_EQUAL (cmsg_composite_client_num_children (broadcast_client), 2);
@@ -137,14 +111,8 @@ void
 test_broadcast_client__servers_up_before_client_init (void)
 {
     cmsg_client *broadcast_client = NULL;
-    uintptr_t cast_id = 0;
 
-    cast_id = (uintptr_t) 1;
-    pthread_create (&server_thread1, NULL, &server_test_thread_run, (void *) cast_id);
-
-    cast_id = (uintptr_t) 2;
-    pthread_create (&server_thread2, NULL, &server_test_thread_run, (void *) cast_id);
-
+    create_servers ();
     sleep (2);
 
     broadcast_client = cmsg_broadcast_client_new (CMSG_DESCRIPTOR (cmsg, test), "cmsg-test",
@@ -160,104 +128,4 @@ test_broadcast_client__servers_up_before_client_init (void)
     cmsg_broadcast_client_destroy (broadcast_client);
 
     stop_servers_and_wait ();
-}
-
-static void *
-client_test_thread_run (void *unused)
-{
-    cmsg_client *client = NULL;
-    int ret = 0;
-    cmsg_uint32_msg send_msg = CMSG_UINT32_MSG_INIT;
-    cmsg_uint32_msg *recv_msg = NULL;
-
-
-    client = cmsg_create_client_tipc_rpc ("cmsg-test", TEST_CLIENT_TIPC_ID,
-                                          TIPC_CLUSTER_SCOPE, CMSG_DESCRIPTOR (cmsg, test));
-
-    CMSG_SET_FIELD_VALUE (&send_msg, value, 123);
-
-    ret = cmsg_test_api_broadcast_client_test (client, &send_msg, &recv_msg);
-
-    NP_ASSERT_EQUAL (ret, CMSG_RET_OK);
-    NP_ASSERT_NOT_NULL (recv_msg);
-    NP_ASSERT_EQUAL (recv_msg->value, 456);
-
-    CMSG_FREE_RECV_MSG (recv_msg);
-
-    cmsg_destroy_client_and_transport (client);
-
-    return 0;
-}
-
-void
-cmsg_test_impl_broadcast_client_test (const void *service, const cmsg_uint32_msg *recv_msg)
-{
-    cmsg_uint32_msg send_msg = CMSG_UINT32_MSG_INIT;
-
-    impl_function_hit = true;
-
-    NP_ASSERT_EQUAL (recv_msg->value, 123);
-
-    CMSG_SET_FIELD_VALUE (&send_msg, value, 456);
-
-    cmsg_test_server_broadcast_client_testSend (service, &send_msg);
-}
-
-/**
- * First initialise a broadcast client. Then create a standard cmsg client
- * and confirm that the client can connect and send to the server of the
- * broadcast client.
- */
-void
-test_broadcast_client__client_can_send_to_broadcast_client (void)
-{
-    static pthread_t client_thread;
-    cmsg_server *server = NULL;
-    fd_set read_fds;
-    struct timeval tv = { 5, 0 };
-    int recv_fd = -1;
-    int ret = -1;
-    eventfd_t value;
-    int *newfd_ptr = NULL;
-    cmsg_server_accept_thread_info *info;
-
-    server = cmsg_create_server_tipc_rpc ("cmsg-test", TEST_CLIENT_TIPC_ID,
-                                          TIPC_CLUSTER_SCOPE, CMSG_SERVICE (cmsg, test));
-
-    ret = cmsg_server_accept_thread_init (server);
-    NP_ASSERT_EQUAL (ret, CMSG_RET_OK);
-
-    info = server->accept_thread_info;
-
-    pthread_create (&client_thread, NULL, &client_test_thread_run, NULL);
-
-    FD_ZERO (&read_fds);
-    FD_SET (info->accept_sd_eventfd, &read_fds);
-
-    select (info->accept_sd_eventfd + 1, &read_fds, NULL, NULL, &tv);
-    NP_ASSERT_EQUAL (g_async_queue_length (info->accept_sd_queue), 1);
-
-    /* clear notification */
-    TEMP_FAILURE_RETRY (eventfd_read (info->accept_sd_eventfd, &value));
-
-    newfd_ptr = g_async_queue_try_pop (info->accept_sd_queue);
-    recv_fd = *newfd_ptr;
-    CMSG_FREE (newfd_ptr);
-
-    /* Since we are using TIPC the first message will be the
-     * CMSG_MSG_TYPE_CONN_OPEN message */
-    ret = cmsg_server_receive (server, recv_fd);
-    NP_ASSERT_EQUAL (ret, 0);
-
-    /* The next message should be the CMSG_MSG_TYPE_METHOD_REQ
-     * message */
-    ret = cmsg_server_receive (server, recv_fd);
-    NP_ASSERT_EQUAL (ret, 0);
-
-    close (recv_fd);
-
-    NP_ASSERT_TRUE (impl_function_hit);
-
-    pthread_join (client_thread, NULL);
-    cmsg_destroy_server_and_transport (server);
 }
