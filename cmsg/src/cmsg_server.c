alliedtelesis/cmsg

commit 1b0ebd778e0f9279356ce7179a064d67dc397a60

ENGBL-1307: Improve thread cancellation logic exposed in tests
Sporadically the functional tests are failing due to a socket leak.
From debugging it was found there were two possible places this could
occur.

- The cancellation points around the cmsg_server_accept call were not
inclusive enough meaning that we could cancel just after accepting the
socket and leak it.
- The logic in '_clear_accept_sd_queue' is wrong. The queue stores pointers
to integers, not integers stored in a pointer value.

This code has not been modified in a while, however recent changes in
the library seem to have caused the tests to fail more frequently.

Change-Id: I24e1de839919ea143eeaac7e34ddc0d6aaf132f0
Reviewed-by: John Thompson <john.thompson@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>
Signed-off-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/src/cmsg_server.c
@@ -2065,20 +2065,19 @@ cmsg_server_accept_thread (void *_server)
     {
         select (listen_socket + 1, &read_fds, NULL, NULL, NULL);
 
+        pthread_setcancelstate (PTHREAD_CANCEL_DISABLE, NULL);
         newfd = cmsg_server_accept (server, listen_socket);
         if (newfd >= 0)
         {
             /* Explicitly set where the thread can be cancelled. This ensures no
              * sockets can be leaked if the thread is cancelled after accepting
              * a connection. */
-            pthread_setcancelstate (PTHREAD_CANCEL_DISABLE, NULL);
             newfd_ptr = CMSG_CALLOC (1, sizeof (int));
             *newfd_ptr = newfd;
             g_async_queue_push (info->accept_sd_queue, newfd_ptr);
-            pthread_setcancelstate (PTHREAD_CANCEL_ENABLE, NULL);
-
             TEMP_FAILURE_RETRY (eventfd_write (info->accept_sd_eventfd, 1));
         }
+        pthread_setcancelstate (PTHREAD_CANCEL_ENABLE, NULL);
     }
 
     pthread_exit (NULL);
@@ -2092,7 +2091,8 @@ cmsg_server_accept_thread (void *_server)
 static void
 _clear_accept_sd_queue (gpointer data)
 {
-    close (GPOINTER_TO_INT (data));
+    int *fd_ptr = (int *) data;
+    close (*fd_ptr);
     CMSG_FREE (data);
 }
 

commit 5f12e8a3e21bdb557cb4eb9ed204fa6b4f13fc96

CR-71767: Fix deadlock on accept thread cancellation
There are a couple of issues with the current select() usage in this function:

1. We are not setting the read_fds variable each time before calling select() (it is
modified after each call).

2. We don't check the return value of select.

This means that currently the select call can return when there is actually
no connection to accept. Since the socket is blocking the underlying accept
call blocks forever and since we have disabled the thread cancellation the
process deadlocks indefinitely. Note that before the previous commit the thread
could be cancelled even though the thread was blocked in the underlying accept()
call.

To avoid this we now switch to the much simpler (and more efficient) poll()
function and ensure we check the return value before calling accept.

fixes: commit 1b0ebd7 ("ENGBL-1307: Improve thread cancellation logic exposed in tests")

Change-Id: Ia923a593a59d267072248bffbf186068a49a110f
Reviewed-by: John Thompson <john.thompson@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

cmsg/src/cmsg_server.c
@@ -2055,29 +2055,30 @@ cmsg_server_accept_thread (void *_server)
     cmsg_server_accept_thread_info *info = server->accept_thread_info;
     int listen_socket = cmsg_server_get_socket (server);
     int newfd = -1;
-    fd_set read_fds;
     int *newfd_ptr;
-
-    FD_ZERO (&read_fds);
-    FD_SET (listen_socket, &read_fds);
+    struct pollfd pfd = {
+        .events = POLLIN,
+        .fd = listen_socket,
+    };
 
     while (1)
     {
-        select (listen_socket + 1, &read_fds, NULL, NULL, NULL);
-
-        pthread_setcancelstate (PTHREAD_CANCEL_DISABLE, NULL);
-        newfd = cmsg_server_accept (server, listen_socket);
-        if (newfd >= 0)
+        if (TEMP_FAILURE_RETRY (poll (&pfd, 1, -1)) > 0)
         {
-            /* Explicitly set where the thread can be cancelled. This ensures no
-             * sockets can be leaked if the thread is cancelled after accepting
-             * a connection. */
-            newfd_ptr = CMSG_CALLOC (1, sizeof (int));
-            *newfd_ptr = newfd;
-            g_async_queue_push (info->accept_sd_queue, newfd_ptr);
-            TEMP_FAILURE_RETRY (eventfd_write (info->accept_sd_eventfd, 1));
+            pthread_setcancelstate (PTHREAD_CANCEL_DISABLE, NULL);
+            newfd = cmsg_server_accept (server, listen_socket);
+            if (newfd >= 0)
+            {
+                /* Explicitly set where the thread can be cancelled. This ensures no
+                 * sockets can be leaked if the thread is cancelled after accepting
+                 * a connection. */
+                newfd_ptr = CMSG_CALLOC (1, sizeof (int));
+                *newfd_ptr = newfd;
+                g_async_queue_push (info->accept_sd_queue, newfd_ptr);
+                TEMP_FAILURE_RETRY (eventfd_write (info->accept_sd_eventfd, 1));
+            }
+            pthread_setcancelstate (PTHREAD_CANCEL_ENABLE, NULL);
         }
-        pthread_setcancelstate (PTHREAD_CANCEL_ENABLE, NULL);
     }
 
     pthread_exit (NULL);