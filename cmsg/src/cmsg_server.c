alliedtelesis/cmsg

commit 23ecbf3502910772f45c41887873aa0b60c68879

ENGBL-921: Add forwarding server
Add a forwarding server concept that can be used in conjunction with
the forwarding client added in the previous patch. Basically this
provides a mechanism for user application code to process a received
CMSG message buffer without having to use a user defined transport.

Change-Id: I8e6ea450093450d0ad191de6256659b9afb254e0
Reviewed-by: John Thompson <john.thompson@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>
Signed-off-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>

cmsg/src/cmsg_server.c
@@ -2602,3 +2602,73 @@ cmsg_server_close_accepted_socket (cmsg_server *server, int socket)
     shutdown (socket, SHUT_RDWR);
     close (socket);
 }
+
+/**
+ * Creates a forwarding server. This should be used to process the data
+ * sent by a forwarding client.
+ *
+ * @param descriptor - The service for the server.
+ *
+ * @returns A pointer to the server on success, NULL otherwise.
+ */
+cmsg_server *
+cmsg_create_server_forwarding (const ProtobufCService *service)
+{
+    cmsg_transport *transport = NULL;
+    cmsg_server *server = NULL;
+
+    transport = cmsg_transport_new (CMSG_TRANSPORT_FORWARDING);
+    if (transport == NULL)
+    {
+        CMSG_LOG_GEN_ERROR ("Could not create transport for forwarding server\n");
+        return NULL;
+    }
+
+    server = cmsg_server_new (transport, service);
+    if (server == NULL)
+    {
+        cmsg_transport_destroy (transport);
+        syslog (LOG_ERR, "Could not create forwarding server");
+        return NULL;
+    }
+
+    return server;
+}
+
+/**
+ * Process the received message for the forwarding server.
+ *
+ * @param server - The forwarding server.
+ * @param data - The received message.
+ * @param length - The length of the received message.
+ * @param user_data - Pointer to data that the caller wishes to access in the IMPL function.
+ */
+void
+cmsg_forwarding_server_process (cmsg_server *server, const uint8_t *data, uint32_t length,
+                                void *user_data)
+{
+    struct cmsg_forwarding_server_data recv_data = {
+        .msg = data,
+        .len = length,
+        .pos = 0,
+        .user_data = user_data,
+    };
+
+    cmsg_transport_forwarding_user_data_set (server->_transport, &recv_data);
+    cmsg_server_receive (server, 1);
+}
+
+/**
+ * Get the user data supplied to the 'cmsg_forwarding_server_process' call.
+ * Note that this data is only available while the message is being processed,
+ * i.e. inside an IMPL function.
+ *
+ * @param server - The server to get the data for.
+ *
+ * @returns Pointer to the supplied user data.
+ */
+void *
+cmsg_forwarding_server_user_data_get (cmsg_server *server)
+{
+    return server->_transport->udt_info.data;
+}

cmsg/src/transport/cmsg_transport_forwarding.c
@@ -27,12 +27,41 @@ cmsg_transport_forwarding_client_send (cmsg_transport *transport, void *buff, in
 {
     struct forwarding_info *info = transport->udt_info.data;
 
-    if (info->send_f (info->user_data, buff, length) < 0)
+    if (!info->send_f (info->user_data, buff, length))
     {
         return -1;
     }
 
     return length;
+}
+
+static int
+cmsg_transport_forwarding_recv_wrapper (cmsg_transport *transport, int sock, void *msg,
+                                        int len, int flags)
+{
+    struct forwarding_info *info = transport->udt_info.data;
+    struct cmsg_forwarding_server_data *recv_data = info->user_data;
+
+    /* Check whether there is any data to read */
+    if (recv_data->pos >= recv_data->len)
+    {
+        return -1;
+    }
+
+    if (recv_data->pos + len > recv_data->len)
+    {
+        len = recv_data->len - recv_data->pos;
+    }
+
+    memcpy (msg, &recv_data->msg[recv_data->pos], len);
+
+    /* If we are only peeking at the data then don't increase the buffer position */
+    if (!(flags & MSG_PEEK))
+    {
+        recv_data->pos += len;
+    }
+
+    return len;
 }
 
 void
@@ -47,6 +76,9 @@ cmsg_transport_forwarding_init (cmsg_transport *transport)
 
     transport->udt_info.functions.client_send = cmsg_transport_forwarding_client_send;
     transport->udt_info.functions.destroy = cmsg_transport_forwarding_client_destroy;
+
+    transport->udt_info.functions.server_recv = cmsg_transport_server_recv;
+    transport->udt_info.functions.recv_wrapper = cmsg_transport_forwarding_recv_wrapper;
 
     transport->udt_info.data = CMSG_CALLOC (1, sizeof (struct forwarding_info));
 }

cmsg/src/transport/cmsg_transport_private.h
@@ -27,6 +27,14 @@
                 __LINE__, transport->parent_obj_id, transport->tport_id, ## __VA_ARGS__); \
     } while (0)
 
+struct cmsg_forwarding_server_data
+{
+    const uint8_t *msg;
+    size_t len;
+    size_t pos;
+    void *user_data;
+};
+
 void cmsg_transport_tcp_init (cmsg_transport *transport);
 void cmsg_transport_oneway_tcp_init (cmsg_transport *transport);
 void cmsg_transport_oneway_cpumail_init (cmsg_transport *transport);

cmsg/include/cmsg/cmsg_server.h
@@ -288,4 +288,9 @@ int32_t cmsg_server_crypto_enable (cmsg_server *server, crypto_sa_create_func_t 
 bool cmsg_server_crypto_enabled (cmsg_server *server);
 void cmsg_server_close_accepted_socket (cmsg_server *server, int socket);
 
+cmsg_server *cmsg_create_server_forwarding (const ProtobufCService *service);
+void cmsg_forwarding_server_process (cmsg_server *server, const uint8_t *data,
+                                     uint32_t length, void *user_data);
+void *cmsg_forwarding_server_user_data_get (cmsg_server *server);
+
 #endif /* __CMSG_SERVER_H_ */

cmsg/include/cmsg/cmsg_transport.h
@@ -86,7 +86,7 @@ typedef struct _cmsg_udt_info_s
     void *data;
 } cmsg_udt_info;
 
-typedef int (*cmsg_forwarding_transport_send_f) (void *user_data, void *buff, int length);
+typedef bool (*cmsg_forwarding_transport_send_f) (void *user_data, void *buff, int length);
 
 typedef union _cmsg_transport_config_u
 {

cmsg/test/functional/client_forwarding_tests.c
@@ -12,6 +12,7 @@
 
 static void *test_ptr;
 static bool func_called = false;
+static bool impl_called = false;
 
 /**
  * Common functionality to run before each test case.
@@ -37,16 +38,23 @@ tear_down (void)
 void
 cmsg_test_impl_simple_forwarding_test (const void *service, const cmsg_bool_msg *recv_msg)
 {
-    /* Should not be hit as client is forwarding to a user defined function */
-    NP_FAIL;
+    impl_called = true;
 }
 
-static int
+static bool
 send_func (void *user_data, void *buff, int length)
 {
     NP_ASSERT_PTR_EQUAL (user_data, test_ptr);
     func_called = true;
-    return 0;
+
+    NP_ASSERT_FALSE (impl_called);
+    cmsg_server *server = cmsg_create_server_forwarding (CMSG_SERVICE (cmsg, test));
+    cmsg_forwarding_server_process (server, buff, length, NULL);
+    cmsg_destroy_server_and_transport (server);
+    NP_ASSERT_TRUE (impl_called);
+    impl_called = false;
+
+    return true;
 }
 
 void

commit 4c411cd101b8081f63db4862acdf4c66b438e6a2

CR-72214,ENGBL-921: Fix 'cmsg_forwarding_server_user_data_get' function
The pointer stored on the transport is to the received data structure,
rather than the user supplied data we wish to return.

fixes: commit 23ecbf3 ("ENGBL-921: Add forwarding server")

Change-Id: Ibf736c1af9697b5db0394a04739f66893a8a1bb0
Reviewed-by: John Thompson <john.thompson@alliedtelesis.co.nz>
Reviewed-by: Kyeong Yoo <kyeong.yoo@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

cmsg/src/cmsg_server.c
@@ -2670,5 +2670,9 @@ cmsg_forwarding_server_process (cmsg_server *server, const uint8_t *data, uint32
 void *
 cmsg_forwarding_server_user_data_get (cmsg_server *server)
 {
-    return server->_transport->udt_info.data;
+    struct cmsg_forwarding_server_data *recv_data;
+
+    recv_data = cmsg_transport_forwarding_user_data_get (server->_transport);
+
+    return recv_data->user_data;
 }

cmsg/src/transport/cmsg_transport_forwarding.c
@@ -97,3 +97,10 @@ cmsg_transport_forwarding_user_data_set (cmsg_transport *transport, void *user_d
     struct forwarding_info *info = transport->udt_info.data;
     info->user_data = user_data;
 }
+
+void *
+cmsg_transport_forwarding_user_data_get (cmsg_transport *transport)
+{
+    struct forwarding_info *info = transport->udt_info.data;
+    return info->user_data;
+}

cmsg/src/transport/cmsg_transport_private.h
@@ -73,5 +73,6 @@ void cmsg_transport_tcp_cache_set (struct in_addr *address, bool present);
 void cmsg_transport_forwarding_func_set (cmsg_transport *transport,
                                          cmsg_forwarding_transport_send_f send_func);
 void cmsg_transport_forwarding_user_data_set (cmsg_transport *transport, void *user_data);
+void *cmsg_transport_forwarding_user_data_get (cmsg_transport *transport);
 
 #endif /* __CMSG_TRANSPORT_PRIVATE_H_ */